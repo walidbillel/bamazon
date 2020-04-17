9bd39753c5812bad78fc1294ce5e5e26a9607ac2
willou <walid.boubguira@gmail.com>
2020-04-17 18:36:09 -0400

    refresh

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,149 +3,166 @@ var inquirer = require("inquirer");
 
 // Connecting to the database
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+  // Your port; if not 3306
+  port: 3306,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "root",
-    database: "bamazon"
+  // Your password
+  password: "root",
+  database: "bamazon",
 });
 
 // When Connected print connection id then display inventory
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("\nconnected as id " + connection.threadId + "\n");
+  if (err) throw err;
+  console.log("\nconnected as id " + connection.threadId + "\n");
 
-    console.log("Loding All The Available Products..........\n");
+  console.log("Loding All The Available Products..........\n");
 
-    setTimeout(displayInventory, 2000);
-
-
+  setTimeout(displayInventory, 2000);
 });
 
 function displayInventory() {
+  connection.query(
+    "SELECT item_id, product_name, price FROM products",
+    function (err, res) {
+      if (err) throw err;
+      // Looping over the results to print things neatly
 
-    
+      console.log(
+        "------------------------------------------------------------------------------------\n"
+      );
 
-    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
-        if (err) throw err;
-        // Looping over the results to print things neatly
+      var logVar = "";
+      for (var i = 0; i < res.length; i++) {
+        logVar = "";
+        logVar += "Item ID: " + res[i].item_id + "  || ";
+        logVar += "Product Name: " + res[i].product_name + "  || ";
+        logVar += "Price: $" + res[i].price + "\n";
 
-        console.log('------------------------------------------------------------------------------------\n');
+        console.log(logVar);
+      }
 
-        var logVar = '';
-        for (var i = 0; i < res.length; i++) {
-            logVar = '';
-            logVar += 'Item ID: ' + res[i].item_id + '  || ';
-            logVar += 'Product Name: ' + res[i].product_name + '  || ';
-            logVar += 'Price: $' + res[i].price + '\n';
+      console.log(
+        "--------------------------------------------------------------------------------------\n"
+      );
 
-            console.log(logVar);
-        }
-
-        console.log("--------------------------------------------------------------------------------------\n");
-
-        setTimeout(promptUser, 2000);
-
-    });
+      setTimeout(promptUser, 2000);
+    }
+  );
 }
 
 //   Asking the user what they want to do using inquirer
 function promptUser() {
-
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "itemId",
-            message: "Please Type in the Item ID of the product you would like to buy! ",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "itemId",
+        message:
+          "Please Type in the Item ID of the product you would like to buy! ",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
         },
+      },
 
-        {
-            type: "input",
-            name: "itemQuantity",
-            message: "How Many Would You like today??",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
+      {
+        type: "input",
+        name: "itemQuantity",
+        message: "How Many Would You like today??",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        },
+      },
+    ])
+    .then(function (answer) {
+      // We Want to store those answers in  variables
+      var itemIdSelected = answer.itemId;
+      var itemQuantityEntered = answer.itemQuantity;
+
+      // Checking up if the id selected exists in the inventory
+      connection.query(
+        "SELECT * FROM products WHERE ?",
+        { item_id: itemIdSelected },
+        function (err, data) {
+          if (err) throw err;
+
+          // If the id the user selected doesnt exist
+          if (data.length === 0) {
+            console.log("Error: Invalid Item ID, Please Select A valid one!");
+
+            setTimeout(displayInventory, 2000);
+
+            // We do checking the quantity wanted and comparing it ....etc
+          } else {
+            // Store the product data in a variable
+            var productSelectedData = data[0];
+
+            // compare it to the quantity available if it's lower or equal we do the updates
+            if (itemQuantityEntered <= productSelectedData.stock_quantity) {
+              console.log(
+                "\nCongratulations, the product quantity you requested is in stock! Placing order....."
+              );
+              //    Updating our bamazon database
+              connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                  {
+                    stock_quantity:
+                      productSelectedData.stock_quantity - itemQuantityEntered,
+                  },
+                  {
+                    item_id: itemIdSelected,
+                  },
+                ],
+                function (err, data) {
+                  if (err) throw err;
+
+                  // logging information for the user
+                  console.log(
+                    "Your order has been placed! Your total is $" +
+                      (productSelectedData.price * itemQuantityEntered).toFixed(
+                        2
+                      )
+                  );
+                  console.log(
+                    "Order Details: " +
+                      itemQuantityEntered +
+                      " " +
+                      productSelectedData.product_name
+                  );
+                  console.log("Thank you for shopping with us!");
+                  console.log(
+                    "\n---------------------------------------------------------------------\n"
+                  );
+
+                  // end connection
+                  connection.end();
                 }
-                return false;
-            }
-        }
+              );
 
-    ]).then(function (answer) {
-
-        // We Want to store those answers in  variables 
-        var itemIdSelected = answer.itemId;
-        var itemQuantityEntered = answer.itemQuantity;
-
-
-        
-
-        // Checking up if the id selected exists in the inventory
-        connection.query("SELECT * FROM products WHERE ?", { item_id: itemIdSelected }, function (err, data) {
-
-            if (err) throw err;
-
-            // If the id the user selected doesnt exist
-            if (data.length === 0) {
-                console.log("Error: Invalid Item ID, Please Select A valid one!");
-
-                setTimeout(displayInventory, 2000);
-
-
-                // We do checking the quantity wanted and comparing it ....etc
+              // IF Qantity enetered is more than inventory quantity log and prompt user again
             } else {
-                // Store the product data in a variable
-                var productSelectedData = data[0];
+              console.log("\nSorry, not enough quantity in our inventory.");
+              console.log("Please modify your order.");
+              console.log(
+                "\n---------------------------------------------------------------------\n"
+              );
 
-            
-                // compare it to the quantity available if it's lower or equal we do the updates
-                if (itemQuantityEntered <= productSelectedData.stock_quantity) {
-                    console.log('\nCongratulations, the product quantity you requested is in stock! Placing order.....');
-                //    Updating our bamazon database
-                    connection.query("UPDATE products SET ? WHERE ?", [{
-                        stock_quantity: productSelectedData.stock_quantity - itemQuantityEntered
-                    },
-                    {
-                        item_id: itemIdSelected
-                    }], function (err, data) {
-                        if (err) throw err;
-
-                        // logging information for the user
-                        console.log('Your order has been placed! Your total is $' + (productSelectedData.price * itemQuantityEntered).toFixed(2));
-                        console.log( "Order Details: " + itemQuantityEntered + " " + productSelectedData.product_name);
-                        console.log('Thank you for shopping with us!');
-                        console.log("\n---------------------------------------------------------------------\n");
-
-                        // end connection
-                        connection.end();
-
-
-                    });
-
-                    // IF Qantity enetered is more than inventory quantity log and prompt user again
-                } else {
-
-                    console.log('\nSorry, not enough quantity in our inventory.');
-                    console.log('Please modify your order.');
-                    console.log("\n---------------------------------------------------------------------\n");
-
-                    setTimeout(promptUser, 2000);
-                }
-
+              setTimeout(promptUser, 2000);
             }
-        });
-
+          }
+        }
+      );
     });
 }
--- bamazonManager.js
+++ bamazonManager.js
@@ -3,261 +3,298 @@ var inquirer = require("inquirer");
 
 // Connecting to the database
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+  // Your port; if not 3306
+  port: 3306,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "root",
-    database: "bamazon"
+  // Your password
+  password: "root",
+  database: "bamazon",
 });
 
 // When Connected print connection id then display inventory
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("\nconnected as id " + connection.threadId + "\n");
+  if (err) throw err;
+  console.log("\nconnected as id " + connection.threadId + "\n");
 
-    console.log("Loding All The Available Products..........\n");
+  console.log("Loding All The Available Products..........\n");
 
-    setTimeout(displayInventory, 2000);
-
+  setTimeout(displayInventory, 2000);
 });
-
 
 // Function that Prompt the Manager
 function promptManager() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Here are the options Manager, what would you like to do?",
+        name: "thingToDo",
+        choices: [
+          "View Products For Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product",
+        ],
+      },
+    ])
+    .then(function (answer) {
+      // storing the manager selection
+      var managerPick = answer.thingToDo;
 
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "Here are the options Manager, what would you like to do?",
-            name: "thingToDo",
-            choices: ["View Products For Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
-        }
-    ]).then(function (answer) {
+      // Applying the switch method for each case
+      switch (managerPick) {
+        case "View Products For Sale":
+          displayInventory();
+          break;
 
-        // storing the manager selection 
-        var managerPick = answer.thingToDo;
+        case "View Low Inventory":
+          displayLowInventory();
+          break;
 
-        // Applying the switch method for each case
-        switch (managerPick) {
+        case "Add to Inventory":
+          addInventory();
+          break;
 
-            case "View Products For Sale":
-                displayInventory();
-                break;
+        case "Add New Product":
+          addNewProduct();
+          break;
 
-            case "View Low Inventory":
-                displayLowInventory();
-                break;
-
-            case "Add to Inventory":
-                addInventory();
-                break;
-
-            case "Add New Product":
-                addNewProduct();
-                break;
-
-            default: console.log("Hey Manager!");
-        }
-
+        default:
+          console.log("Hey Manager!");
+      }
     });
 }
 
 // Function That displays Inventory
 function displayInventory() {
+  // Selecting the items
+  connection.query(
+    "SELECT item_id, product_name, price, stock_quantity FROM products",
+    function (err, res) {
+      if (err) throw err;
 
-   
+      // Looping over the results to print things neatly
+      console.log(
+        "------------------------------------------------------------------------------------\n"
+      );
 
-    // Selecting the items
-    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, res) {
-        if (err) throw err;
+      var logVar = "";
+      for (var i = 0; i < res.length; i++) {
+        logVar = "";
+        logVar += "Item ID: " + res[i].item_id + "  || ";
+        logVar += "Product Name: " + res[i].product_name + "  || ";
+        logVar += "Price: $" + res[i].price + "  || ";
+        logVar += "Stock Quantity: " + res[i].stock_quantity + "\n";
 
-        // Looping over the results to print things neatly
-        console.log('------------------------------------------------------------------------------------\n');
+        console.log(logVar);
+      }
+      console.log(
+        "--------------------------------------------------------------------------------------\n"
+      );
 
-        var logVar = '';
-        for (var i = 0; i < res.length; i++) {
-            logVar = '';
-            logVar += 'Item ID: ' + res[i].item_id + '  || ';
-            logVar += 'Product Name: ' + res[i].product_name + '  || ';
-            logVar += 'Price: $' + res[i].price + '  || ';
-            logVar += 'Stock Quantity: ' + res[i].stock_quantity + '\n';
-
-            console.log(logVar);
-        }
-        console.log("--------------------------------------------------------------------------------------\n");
-
-        // Prompting the manager again
-        setTimeout(promptManager, 2000);
-    });
+      // Prompting the manager again
+      setTimeout(promptManager, 2000);
+    }
+  );
 }
 
 // Function that displays low inventory
 function displayLowInventory() {
+  // Selecting from database inventory where stock_quntity is <= 5
+  connection.query(
+    "SELECT * FROM products WHERE stock_quantity <= 5",
+    function (err, res) {
+      if (err) throw err;
 
-    // Selecting from database inventory where stock_quntity is <= 5
-    connection.query("SELECT * FROM products WHERE stock_quantity <= 5", function (err, res) {
+      console.log(
+        "\nHere Are All The Products with Inventory lower than 5 ...\n"
+      );
 
-        if (err) throw err;
+      console.log(
+        "------------------------------------------------------------------------------------\n"
+      );
 
-        console.log("\nHere Are All The Products with Inventory lower than 5 ...\n");
+      // Looping over for a neat print
+      var logVar = "";
+      for (var i = 0; i < res.length; i++) {
+        logVar = "";
+        logVar += "Item ID: " + res[i].item_id + "  || ";
+        logVar += "Product Name: " + res[i].product_name + "  || ";
+        logVar += "Price: $" + res[i].price + "  || ";
+        logVar += "Stock Quantity: " + res[i].stock_quantity + "\n";
 
-        console.log('------------------------------------------------------------------------------------\n');
+        console.log(logVar);
+      }
 
-        // Looping over for a neat print
-        var logVar = '';
-        for (var i = 0; i < res.length; i++) {
-            logVar = '';
-            logVar += 'Item ID: ' + res[i].item_id + '  || ';
-            logVar += 'Product Name: ' + res[i].product_name + '  || ';
-            logVar += 'Price: $' + res[i].price + '  || ';
-            logVar += 'Stock Quantity: ' + res[i].stock_quantity + '\n';
-
-            console.log(logVar);
-
-        }
-
-        // Prompting the manager again
-        console.log("--------------------------------------------------------------------------------------\n");
-        setTimeout(promptManager, 2000);
-    });
-
-
+      // Prompting the manager again
+      console.log(
+        "--------------------------------------------------------------------------------------\n"
+      );
+      setTimeout(promptManager, 2000);
+    }
+  );
 }
 
 // Function that adds inventory
 function addInventory() {
-
-    // Prompting the manager for the item_id and quantity
-    inquirer.prompt([
-        {
-            type: "input",
-            name: 'productID',
-            message: 'Please Enter the product ID that you would like to add Inventory to?',
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
+  // Prompting the manager for the item_id and quantity
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "productID",
+        message:
+          "Please Enter the product ID that you would like to add Inventory to?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
         },
-        {
-            type: "input",
-            name: 'quantity',
-            message: 'How many would you like to add?',
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
+      },
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to add?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
         },
-    ]).then(function (input) {
+      },
+    ])
+    .then(function (input) {
+      // storing answers
+      var inventoryID = input.productID;
+      var inventoryWillingToAdd = input.quantity;
 
-        // storing answers
-        var inventoryID = input.productID;
-        var inventoryWillingToAdd = input.quantity;
+      // Selecting the item-id we want to do updates on
+      connection.query(
+        "SELECT * FROM products WHERE item_id = " + inventoryID,
+        function (err, res) {
+          if (err) throw err;
 
-        // Selecting the item-id we want to do updates on
-        connection.query('SELECT * FROM products WHERE item_id = ' + inventoryID, function (err, res) {
+          // Adding up to the stock inventory
+          var newInventoryQuantity =
+            res[0].stock_quantity + parseFloat(inventoryWillingToAdd);
 
-            if (err) throw err;
+          // Log Results to the Manager Then Prompting again
+          for (var i = 0; i < res.length; i++) {
+            logVar = "";
+            logVar += "Item ID: " + res[i].item_id + "  || ";
+            logVar += "Product Name: " + res[i].product_name + "  || ";
+            logVar += "New Stock Quantity: " + newInventoryQuantity + "\n";
 
-            // Adding up to the stock inventory
-            var newInventoryQuantity = res[0].stock_quantity + parseFloat(inventoryWillingToAdd);
-            
+            console.log(
+              "\n------------------------------------------------------------------------------------"
+            );
+            console.log(
+              "You Successfully Added " +
+                inventoryWillingToAdd +
+                " " +
+                res[i].product_name +
+                " To The Stock.\n"
+            );
+            console.log(logVar);
 
-            // Log Results to the Manager Then Prompting again
-            for (var i = 0; i < res.length; i++) {
-                logVar = '';
-                logVar += 'Item ID: ' + res[i].item_id + '  || ';
-                logVar += 'Product Name: ' + res[i].product_name + '  || ';
-                logVar += 'New Stock Quantity: ' + newInventoryQuantity + '\n';
+            setTimeout(promptManager, 2000);
+          }
 
-                console.log('\n------------------------------------------------------------------------------------');
-                console.log("You Successfully Added " + inventoryWillingToAdd + " " + res[i].product_name + " To The Stock.\n");
-                console.log(logVar);
-
-                setTimeout(promptManager, 2000);
+          // Updating Database
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              { stock_quantity: newInventoryQuantity },
+              { item_id: inventoryID },
+            ],
+            function (err, res) {
+              if (err) throw err;
             }
-
-             // Updating Database
-            connection.query("UPDATE products SET ? WHERE ?", [
-                { stock_quantity: newInventoryQuantity },
-                { item_id: inventoryID }
-            ], function (err, res) {
-                if (err) throw err;
-
-            });
-        });
+          );
+        }
+      );
     });
-
-
 }
-
 
 function addNewProduct() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "product_name",
+        message: "What is the product name?",
+      },
+      {
+        type: "input",
+        name: "department_name",
+        message: "Which department does the new product belong to?",
+      },
+      {
+        type: "input",
+        name: "price",
+        message: "What is the price per unit?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        },
+      },
+      {
+        type: "input",
+        name: "stock_quantity",
+        message: "How many items are in stock?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        },
+      },
+    ])
+    .then(function (input) {
+      console.log(
+        "\n--------------------------------------------------------------------------------------\n"
+      );
+      console.log("Adding a New Product");
+      console.log(
+        "product_name: " +
+          input.product_name +
+          "\n" +
+          "department_name: " +
+          input.department_name +
+          "\n" +
+          "price: $" +
+          input.price +
+          "\n" +
+          "stock_quantity: " +
+          input.stock_quantity +
+          "\n"
+      );
 
-    inquirer.prompt([
-
+      connection.query(
+        "INSERT INTO products SET ?",
         {
-            type: 'input',
-            name: 'product_name',
-            message: 'What is the product name?',
+          product_name: input.product_name,
+          department_name: input.department_name,
+          price: input.price,
+          stock_quantity: input.stock_quantity,
         },
-        {
-            type: 'input',
-            name: 'department_name',
-            message: 'Which department does the new product belong to?',
-        },
-        {
-            type: 'input',
-            name: 'price',
-            message: 'What is the price per unit?',
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
-        },
-        {
-            type: 'input',
-            name: 'stock_quantity',
-            message: 'How many items are in stock?',
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
+        function (err, res) {
+          if (err) throw err;
+          console.log(
+            "The New Product Was Successfully Added, You Will See it under Item ID: " +
+              res.insertId +
+              "\n"
+          );
+          setTimeout(displayInventory, 2000);
         }
-
-    ]).then(function (input) {
-
-        console.log("\n--------------------------------------------------------------------------------------\n");
-        console.log("Adding a New Product");
-        console.log('product_name: ' + input.product_name + '\n' + 'department_name: ' + input.department_name + '\n' + 'price: $' + input.price + '\n' + 'stock_quantity: ' + input.stock_quantity + '\n');
-
-        connection.query("INSERT INTO products SET ?",
-            {
-                product_name: input.product_name,
-                department_name: input.department_name,
-                price: input.price,
-                stock_quantity: input.stock_quantity
-            }, function (err, res) {
-
-                if (err) throw err;
-                console.log("The New Product Was Successfully Added, You Will See it under Item ID: " + res.insertId + "\n");
-                setTimeout(displayInventory, 2000);
-
-            });
-
+      );
     });
 }
-
